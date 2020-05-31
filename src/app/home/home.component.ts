dd3d088885f0eca5756e62587c19a09696d8234d
Anthony Brignano <abrignano@comcast.net>
2020-05-31 14:45:37 -0400

    add google fonts to be served locally

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,21 +4,9 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  animations: [
-    trigger('signatureAnimation', [
-      state('start', style({
-        opacity: 0
-      })),
-      state('end', style({
-        opacity: 1
-      })),
-      transition('start=>end', animate('3000ms')),
-    ]),
-  ]
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, AfterViewInit {
-  signatureAnimationState = 'start';
 
   constructor() { }
 
@@ -26,7 +14,6 @@ export class HomeComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.signatureAnimationState = 'end';
   }
 
 }
